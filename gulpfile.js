76507d552190da211b00f8bf65d22e25c5743fd7
trabur <travis.burandt@gmal.com>
2020-04-13 14:54:23 -0500

    clean

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp");
-var babel = require("gulp-babel");
-
-gulp.task("default", function () {
-  return gulp.src("server/app.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
-});
